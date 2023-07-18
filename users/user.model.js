3e9ad125ddf109b5597b660ef44d2e1b814ce73b
dlorcav77 <diego.lorcaveliz@gmail.com>
2023-07-18 19:19:42 -0400

    modify user avatar

--- users/user.model.js
+++ users/user.model.js
@@ -17,9 +17,9 @@ const findLogin = async(email) => {
     return rows[0];
 };
 
-const  create = async({name, lastName, email, password}) =>{
-    const query = "INSERT INTO users (name, lastName, email, password)VAlUES($1,$2,$3,$4) RETURNING *";
-    const {rows} = await pool.query(query, [name, lastName, email, password]);
+const  create = async({name, lastName, email, password, img_avatar}) =>{
+    const query = "INSERT INTO users (name, lastName, email, password, img_avatar)VAlUES($1,$2,$3,$4,$5) RETURNING *";
+    const {rows} = await pool.query(query, [name, lastName, email, password, img_avatar]);
     return rows[0];
 };
 
