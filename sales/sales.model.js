327a41f294da0779e86b23828503b61cc708b3dc
Arekkusu17 <fernn.alex@gmail.com>
2023-07-17 20:43:17 -0400

    modify sales model and controller

--- sales/sales.model.js
+++ sales/sales.model.js
@@ -1,38 +1,24 @@
-import {pool} from "../db/conn.js";
+import { pool } from "../db/conn.js";
 
-const findAll = async() => {
-    const {rows} = await pool.query("select * from sales");
-    return rows;
-};
+// const findAll = async () => {
+//     const { rows } = await pool.query("select * from sales");
+//     return rows;
+// };
 
-const findOne = async(id) => {
-    const query  = "select * from sales WHERE id = $1";
-    const {rows} = await pool.query(query, [id]);
-    return rows[0];
-};
+// const findOne = async (id) => {
+//     const query = "select * from sales WHERE id = $1";
+//     const { rows } = await pool.query(query, [id]);
+//     return rows[0];
+// };
 
-const  create = async({id_user, date, total}) =>{
+const create = async ({ id_user, date, total }) => {
     const query = "INSERT INTO sales (id_user, date, total)VAlUES($1,$2,$3) RETURNING *";
-    const {rows} = await pool.query(query, [id_user, date, total]);
-    return rows[0];
-};
-
-const  update = async(id, {id_user, date, total}) =>{
-    const query = "UPDATE sales SET id_user = $1, date = $2, total = $3 WHERE id = $4 RETURNING *";
-    const {rows} = await pool.query(query, [id_user, date, total, id]);
-    return rows[0];
-};
-
-const  remove = async(id) =>{
-    const query = "DELETE FROM sales WHERE id = $1 RETURNING *";
-    const {rows} = await pool.query(query, [id]);
+    const { rows } = await pool.query(query, [id_user, date, total]);
     return rows[0];
 };
 
 export const salesModel = {
-    findAll,
-    findOne,
-    create,
-    update,
-    remove,
+    // findAll,
+    // findOne,
+    create
 };
