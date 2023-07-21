b1fa3ade9846696dd15b1fba9d12476ead62d99d
Arekkusu17 <fernn.alex@gmail.com>
2023-07-21 11:21:39 -0400

    modify connection parameters to use url if available

--- db/conn.js
+++ db/conn.js
@@ -4,9 +4,27 @@ dotenv.config();
 import pkg from "pg";
 const { Pool } = pkg;
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.PG_URL;
 
-export const pool = new Pool({
-    connectionString,
-    allowExitOnIdle: true
-});
+export const pool = connectionString ?
+    new Pool({
+        connectionString,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+        allowExitOnIdle: true
+    }) : new Pool({
+        allowExitOnIdle: true
+    });
+
+try {
+    // Realiza una consulta sencilla para verificar la conexión
+    const res = await pool.query("SELECT NOW()");
+    // Si la conexión es exitosa, imprime la fecha actual devuelta por la consulta
+    console.log(
+        "Conexión exitosa. Hora actual en la base de datos:",
+        res.rows[0].now
+    );
+} catch (error) {
+    console.log(error);
+}
