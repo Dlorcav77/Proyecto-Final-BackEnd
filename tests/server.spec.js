3604ab807fda0bae8360877c6b9ddeb756efe417
Arekkusu17 <fernn.alex@gmail.com>
2023-07-20 20:45:31 -0400

    update tests

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,7 +1,5 @@
 import request from "supertest";
 import server from "../index.js";
-import { signJWT } from "../utils/jwt.utils.js";
-
 
 describe("Test operations for users routes", () => {
 
@@ -17,7 +15,6 @@ describe("Test operations for users routes", () => {
 
     const response = await request(server).post("/api/v1/users").send(testUser);
     const status = response.statusCode;
-
 
     expect(status).toBe(201)
   })
@@ -40,16 +37,27 @@ describe("Test operations for favorites routes", () => {
   })
 })
 
-
-
-describe("Test operations for rating", () => {
-  it("DELETE /favorites/id returns statusCode 401 when the Token is invalid", async () => {
-    const randomizeId = Math.floor(Math.random() * 999)
-    const response = await request(server).delete(`/api/v1/favorites/${randomizeId}`).set('Authorization', `Bearer token`).send()
+describe("Test operations for classes routes", () => {
+  it("GET /classes returns statusCode 200 and the data type returned is an array of more than one item", async () => {
+    const response = await request(server).get("/api/v1/classes").send();
     const status = response.statusCode;
+    const classes = response.body.result;
 
-    expect(status).toBe(401)
+    expect(status).toBe(200);
+    expect(classes.length).toBeGreaterThanOrEqual(1)
   })
 })
 
+describe("Test operations for comments routes", () => {
+  it("GET /comments/class/:id returns statusCode 200 when there is not comments on a class", async () => {
+    const testId = 15
+    const response = await request(server).get(`/api/v1/comments/class/${testId}`).send();
+    console.log(response)
 
+    const status = response.statusCode;
+    const classes = response.body.result;
+
+    expect(status).toBe(200);
+    expect(classes.length).toBe(0)
+  })
+})
